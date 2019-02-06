2fe89ce66c4e158724a3d853c09c6568f91eff5d
Sofia Donataccio <srdqty@gmail.com>
2019-02-05 21:37:05 -0500

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Irdis (1.3.1) Sandbox
---
+++ shell.nix
@@ -0,0 +1,19 @@
+let
+  pkgs = import ./nix/nixpkgs {};
+
+  idris = pkgs.idrisPackages.with-packages (with pkgs.idrisPackages; [
+    prelude
+    base
+  ]);
+in
+  pkgs.stdenv.mkDerivation {
+    name = "idris-sandbox";
+
+    buildInputs = [
+      idris
+      pkgs.ncurses
+      pkgs.gmp
+    ];
+
+    shellHook = builtins.readFile ./nix/bash-prompt.sh;
+  }
